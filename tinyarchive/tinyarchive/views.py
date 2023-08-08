a7618b693fd048a9f424edf7fe39500bc2eeba6f
Thanh Nguyen <thanh.nguyen3n@gmail.com>
2023-08-08 16:18:01 -0700

    clean up

--- tinyarchive/tinyarchive/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# This is just the view for the static homepage.
-def home(request):
-    return render(request, "home.html", {})
